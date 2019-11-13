1c4eb5251be2bf378f4afba9410e2b0539d504fe
Landilito <36903709+Landilito@users.noreply.github.com>
2019-11-13 13:24:47 -0400

    Cambios en el string de conexion y formato para la lectura de datos

--- BatchStarter/Program.cs
+++ BatchStarter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,28 @@ namespace BatchStarter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Programa para correr batches");
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = "Server=DESKTOP-J6ACRTS;Database=CIA_Factbook_DB;Trusted_Connection=True";
+
+                connection.Open();
+                SqlCommand query = new SqlCommand("SELECT * FROM City WHERE Name = @CityName", connection);
+                query.Parameters.Add(new SqlParameter("CityName", "Washington"));
+
+                Console.WriteLine("Programa para correr batches");
+
+                using (SqlDataReader reader = query.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0} \t | {1} \t | {2} \t | {3}",
+                        reader[0], reader[1], reader[2], reader[3]));
+                    }
+                }
+                connection.Close();
+            }
+
         }
     }
 }
