cffc99f267715819a400fc7716d653cde14c353c
Karen Arias <44011613+karenbarias@users.noreply.github.com>
2019-11-15 15:39:53 -0400

    horarios

--- BatchStarter/Program.cs
+++ BatchStarter/Program.cs
@@ -13,58 +13,64 @@ namespace BatchStarter
     {
         static void Main(string[] args)
         {
-            int MO = 0;
 
-            using (SqlConnection connection = new SqlConnection())
+            try
             {
-                connection.ConnectionString = "Server=DESKTOP-J6ACRTS;Database=HOLA;Trusted_Connection=True";
-                connection.Open();
-                SqlCommand query = new SqlCommand("SELECT COUNT(*) FROM dbo.adios Where adios1 = 2", connection);
-                //SqlCommand query = new SqlCommand("SELECT @MO = COUNT(*)"
-                //    +"FROM[DB_SMS_Connections].[dbo].[SmppInputMessage] with(nolock)"
-                //    +" Where connectionId = 'S_NEXTEL_MX_LONG_RX1' and date >= DATEADD(MINUTE, -5, GETDATE())", connection);
-                //query.Parameters.Add(new SqlParameter("kk",2));
-
-                Console.WriteLine("Programa para correr batches");
-
-                using (SqlDataReader reader = query.ExecuteReader())
+                using (SqlConnection connection = new SqlConnection())
                 {
-                    while (reader.Read())
-                    { 
-                        MO = (int)reader[0];
+                    Console.WriteLine("Programa para correr batches");
 
-                        Console.WriteLine(String.Format("El valor es {0}", MO));
+                    var time = DateTime.Now.Hour;
+
+                    connection.ConnectionString = "Server=192.168.100.42;Database=DB_SMS_Connections;User ID=usrTableau;Password=kMZdPuTyj7c5RbXn";
+                    connection.Open();
+                    SqlCommand query = new SqlCommand("SELECT COUNT(*)"
+                                    + "FROM[DB_SMS_Connections].[dbo].[SmppInputMessage] with(nolock)"
+                       + " Where connectionId = 'S_NEXTEL_MX_LONG_RX1' and date >= DATEADD(MINUTE, -2, GETDATE())", connection);
+
+                    if (time >= 20)
+                    {
+                        query = new SqlCommand("SELECT COUNT(*)"
+                                        + "FROM[DB_SMS_Connections].[dbo].[SmppInputMessage] with(nolock)"
+                           + " Where connectionId = 'S_NEXTEL_MX_LONG_RX1' and date >= DATEADD(MINUTE, -10, GETDATE())", connection);
                     }
+                    else if (time >= 0 && time <= 6)
+                    {                        query = new SqlCommand("SELECT COUNT(*)"
+                                        + "FROM[DB_SMS_Connections].[dbo].[SmppInputMessage] with(nolock)"
+                           + " Where connectionId = 'S_NEXTEL_MX_LONG_RX1' and date >= DATEADD(MINUTE, -30, GETDATE())", connection);
+                    }
+
+                    using (SqlDataReader reader = query.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int MO = (int)reader[0];
+
+                            Console.WriteLine(String.Format("La cantidad de MO es {0}", MO));
+
+                            if (MO >= 1)
+                            {
+                                Console.WriteLine("Connection is up");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Connection is down. Atempting to restart...");
+                                Process batch = new Process();
+                                batch.StartInfo.CreateNoWindow = true;
+                                batch.StartInfo.FileName = @"Restart S_NEXTEL_MX_LONG_RX1.bat";
+                                batch.Start();
+                            }
+                        }
+                    }
+                    connection.Close();
                 }
-                connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocurrio un error \n" + e);
             }
 
-            if(MO > 1)
-            {
-                Console.WriteLine("Connection is up");
-            }
-            else
-            {
-                Console.WriteLine("Connection is down. ");
-                               
-                Process batch = new Process();
-                batch.StartInfo.CreateNoWindow = true;            
-                batch.StartInfo.FileName = @"kk.bat";                
-                batch.Start();
-
-                Console.WriteLine("...");
-
-                ServiceController sc = new ServiceController("TeamViewer");
-                if (sc.Status == ServiceControllerStatus.Running)
-                {
-                    Console.Write("Service is running.");
-                }
-                else
-                {
-                    Console.Write("Service not running.");
-                }
-            }            
-
+            Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
     }
